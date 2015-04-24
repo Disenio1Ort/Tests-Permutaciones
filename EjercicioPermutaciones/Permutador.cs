1011443fb78c4ba4e0413d86e681252602baa72e
Ricardo Szyfer <ricardo.szyfer@kinamic.com>
2015-04-24 10:31:45 -0300

    Ejercicio de Permutaciones

---
+++ EjercicioPermutaciones/Permutador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPermutaciones
+{
+    public static class Permutador
+    {
+        public static IEnumerable<string> Permutar(string palabra)
+        {
+            if (palabra == null)
+                throw new ArgumentNullException("Palabra no puede ser null!");
+
+            var permutaciones = new List<string>();
+            
+            if(palabra.Length == 0)
+            {
+                return permutaciones;
+            }
+            
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                var letra = palabra[i].ToString();
+                var palabraRestante = palabra.Substring(0, i) + palabra.Substring(i + 1, palabra.Length - (i + 1));
+                var permutacionesRestantes = Permutar(palabraRestante);
+
+                if (permutacionesRestantes.Count() > 0)
+                {
+                    foreach (var permutacion in permutacionesRestantes)
+                        permutaciones.Add(letra + permutacion);
+                }
+                else
+                    permutaciones.Add(letra);
+            }
+
+            return permutaciones;
+        }
+    }
+}
---
+++ PermutacionesTest/PermutacionesTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EjercicioPermutaciones
+{
+    public class PermutacionesTest
+    {
+        private string Letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+        [Fact]
+        public void PermutacionesVacioTest()
+        {
+            string vacio = "";
+            var result = Permutador.Permutar(vacio);
+
+            Assert.Equal(new List<string>(), result);
+        }
+
+        [Fact]
+        public void PermutacionesUnaLetraTest()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+
+            string letra = Letras[r.Next(Letras.Length)].ToString();
+            
+            var result = Permutador.Permutar(letra);
+            var expected = new List<string>() { letra };
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void PermutacionesDosLetrasTest()
+        {
+
+            var letra1 = GetLetra();
+            var letra2 = GetLetra();
+            string palabra =  letra1 + letra2;
+
+            var result = Permutador.Permutar(palabra);
+            var expected = new List<string>() { letra1 + letra2 , letra2 + letra1};
+            Console.WriteLine(result.ToString());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void PermutacionesTresLetrasTest()
+        {
+
+            var letra1 = GetLetra();
+            var letra2 = GetLetra();
+            var letra3 = GetLetra();
+            string palabra = letra1 + letra2 + letra3;
+
+            var result = Permutador.Permutar(palabra);
+            var expected = new List<string>() 
+            { 
+                letra1 + letra2 + letra3, 
+                letra1 + letra3 + letra2, 
+                letra2 + letra3 + letra1, 
+                letra2 + letra1 + letra3, 
+                letra3 + letra1 + letra2, 
+                letra3 + letra2 + letra1, 
+            };
+
+            
+            expected.Sort();
+            var resultList = result.ToList();
+            resultList.Sort();
+            Assert.Equal(expected, resultList);
+        }
+
+        [Fact]
+        public void PermutacionesNLetrasTest()
+        {
+            var palabra = "";
+
+            Random r = new Random((int)DateTime.Now.Ticks);
+
+            int largoPalabra = r.Next(4, 10);
+
+            for (int i = 0; i < largoPalabra; i++)
+                palabra += GetLetra();
+            
+            var result = Permutador.Permutar(palabra);
+            
+            var cantPalabrasEsperadas = 1;
+            for(int i = largoPalabra; i > 1; i--)
+                cantPalabrasEsperadas *= i;
+
+            Assert.Equal(cantPalabrasEsperadas, result.Count());
+        }
+        private string GetLetra()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            
+            return Letras[r.Next(Letras.Length)].ToString();
+        }
+    }
+}
